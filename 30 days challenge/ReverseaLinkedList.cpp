33cb2693aaaf193baf03b3d2bd1a139cf5c19c0c
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2022-02-07 21:50:18 +0530

    Reverse a Linked List
    Reverse a Linked List:-Given the head of a singly linked list, write a program to reverse the linked list, and return the head pointer to the reversed list.

---
+++ 30 days challenge/ReverseaLinkedList.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+public:
+
+    ListNode* reverseList(ListNode* &head) {
+
+        if (head == NULL||head->next==NULL)
+            return head;
+
+        ListNode* nnode = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return nnode;
+    }
+};
